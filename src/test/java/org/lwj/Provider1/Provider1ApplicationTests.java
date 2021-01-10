0bf80662ac06bc0cf10b994efced6402cb942ba8
LvWenJin <972950811@qq.com>
2021-01-10 23:18:09 +0800

    分布式提供者

---
+++ src/test/java/org/lwj/Provider1/Provider1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lwj.Provider1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Provider1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
