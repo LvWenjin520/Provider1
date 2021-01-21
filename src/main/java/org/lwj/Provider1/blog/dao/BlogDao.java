ad1eeb3788eaf7be8d2aec1afac6896ae8d0a177
lwj <972950811@qq.com>
2021-01-21 15:43:58 +0800

    获取文章

--- src/main/java/org/lwj/Provider1/blog/dao/BlogDao.java
+++ src/main/java/org/lwj/Provider1/blog/dao/BlogDao.java
@@ -2,11 +2,13 @@ package org.lwj.Provider1.blog.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import entitys.blog.entity.Article;
 
 @Repository
+@Mapper
 public interface BlogDao {
 	//获取所有的文章
 	List<Article> getArticle(String userName);
